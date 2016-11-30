dbdecf5ad8d5bffc91254ac1ff1396acf3602e7c
xiayiwei <yiwei.xia@ele.me>
2016-12-01 01:23:06 +0800

    fix bug

--- myblog/wsgi.py
+++ myblog/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 import sys
-root = os.path.dirname(__file__)                                             #输出当前文件所在文件夹的绝对路径
+root = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(root, '..', 'site-packages'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myblog.settings")
 
